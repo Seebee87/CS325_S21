6f3381519454fc3b025e2c15f472cc4d60431dfb
Seebee87 <55639377+Seebee87@users.noreply.github.com>
2021-04-30 17:24:48 -0400

    Week 5 HW

--- Week 5/Sum.py
+++ Week 5/Sum.py
@@ -1,2 +1,23 @@
-def combination(n,k):
-    
+from copy import deepcopy
+
+def combination_sum_helper(nums, start, result, remainder, combination, length):
+
+    if remainder == 0:
+        if length == len(combination):
+            result.append(deepcopy(combination))
+        return
+    elif remainder < 0:
+        return 
+    for i in range(start, len(nums)):
+        combination.append(nums[i])
+        combination_sum_helper(nums, i+1, result, remainder-nums[i], combination, length)
+        combination.pop()
+
+
+def combination_sum(length, target):
+    result = []
+    key = [1,2,3,4,5,6,7,8,9]
+    combination_sum_helper(key,0, result, target, [], length)
+    print(result)
+
+print(combination_sum(3,9))
